010affba0cd340d200f117b68505421820a9b5bf
Georhaphy <polasants@gmail.com>
2024-10-03 08:25:28 +0700

    Update main.py

--- main.py
+++ main.py
@@ -26,10 +26,10 @@ img_file = st.file_uploader("เปิดไฟล์ภาพ")
 if img_file is not None:    
     file_bytes = np.asarray(bytearray(img_file.read()), dtype=np.uint8)
     img = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
-   
+    result = model.predict(img)
     
    
-result = model.predict(img)
+
 
 
 for detection in result[0].boxes.data:
